f4886d380ed7046b9ca5d48cf0497a5024d89779
edean78 <edean78@gmail.com>
2020-02-02 22:47:30 -0500

    added the routes to the projects.js and project controller in the controllers directory

---
+++ routes/api/projects.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+const projectController = require('../../controllers/projectController');
+
+// Matches with "/api/projects"
+router.route('/')
+    .get(projectController.findAll)
+    .post(projectController.create);
+
+// Matches with "/api/projects/:id"
+router
+    .route('/:id')
+    .get(projectController.findById)
+    .put(projectController.update)
+    .delete(projectController.remove);
+
+module.exports = router;
+    
