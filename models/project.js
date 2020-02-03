db6f1e6243d680f37959ab6919c9281090e6ab40
edean78 <edean78@gmail.com>
2020-02-02 23:05:24 -0500

    created the model for the projects

---
+++ models/project.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const projectSchema = new Schema({
+    week: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    url: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    image: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    desc: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    repoURL: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Project = mongoose.model('Project', projectSchema);
+
+module.exports = Project;
