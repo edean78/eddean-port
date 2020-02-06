7b3f4b9e3192f02a815b314dbfa3ec1dcda136d3
edean78 <edean78@gmail.com>
2020-02-06 00:03:47 -0500

    removed a few routes in the api/projects.js file

--- routes/api/projects.js
+++ routes/api/projects.js
@@ -4,14 +4,6 @@ const projectController = require('../../controllers/projectController');
 // Matches with "/api/projects"
 router.route('/')
     .get(projectController.findAll)
-    .post(projectController.create);
-
-// Matches with "/api/projects/:id"
-router
-    .route('/:id')
-    .get(projectController.findById)
-    .put(projectController.update)
-    .delete(projectController.remove);
 
 module.exports = router;
     
