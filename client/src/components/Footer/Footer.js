f7baecb00b1ca278f8a5fd7995b62ba50a8b31f2
edean78 <edean78@gmail.com>
2020-02-05 23:29:12 -0500

    added rel=noopener to the footer.js file

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -5,11 +5,11 @@ function Footer() {
     return (
         <footer className="page-footer">
             <div className="container" id="media-icons">
-                <a href="https://github.com/edean78" title="Github Profile" target="_blank"><i className="fab fa-github"></i></a>
-                <a href="https://www.linkedin.com/in/edean78/" title="Linked In Profile" target="_blank"><i className="fab fa-linkedin-in"></i></a>
-                <a href="https://www.facebook.com/eddean78" title="Facebook Profile" target="_blank"><i className="fab fa-facebook"></i></a>
-                <a href="https://twitter.com/eddean78" title="Twitter Profile" target="_blank"><i className="fab fa-twitter"></i></a>
-                <a href="https://www.instagram.com/eddeaniii/" title="Instagram Profile" target="_blank"><i className="fab fa-instagram"></i></a>
+                <a href="https://github.com/edean78" title="Github Profile" target="_blank"><i className="fab fa-github" rel="noopener"></i></a>
+                <a href="https://www.linkedin.com/in/edean78/" title="Linked In Profile" target="_blank" rel="noopener"><i className="fab fa-linkedin-in"></i></a>
+                <a href="https://www.facebook.com/eddean78" title="Facebook Profile" target="_blank" rel="noopener"><i className="fab fa-facebook"></i></a>
+                <a href="https://twitter.com/eddean78" title="Twitter Profile" target="_blank" rel="noopener"><i className="fab fa-twitter"></i></a>
+                <a href="https://www.instagram.com/eddeaniii/" title="Instagram Profile" target="_blank" rel="noopener"><i className="fab fa-instagram"></i></a>
             </div>
         </footer>
     );
