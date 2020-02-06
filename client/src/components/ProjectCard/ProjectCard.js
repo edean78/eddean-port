5b7d916fc102bba37e507bdf6143b1b6bc0ff01e
edean78 <edean78@gmail.com>
2020-02-06 10:20:34 -0500

    ProjectCard now works by creating porject object and pull information from it instead of db

--- client/src/components/ProjectCard/ProjectCard.js
+++ client/src/components/ProjectCard/ProjectCard.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import './ProjectCard.css';
 
-function ProjectCard({props}) {
-    console.log({props})
-    return (
-        <div class="card border-info m-auto">
-            <div className="img-container">
-                <img alt={props.title} src={props.image} />
+function ProjectCard({projects}) {
+    console.log(projects)
+
+    const projectsList = projects.map( project => (
+        <div class="card border-info m-auto col-md-3" id="project-card">
+            <div className="img-container" id="project-image">
+                <a href={project.url}>
+                <img alt={project.title} src={project.image}/>
+                </a>
             </div>
-            <div class="card-body">
-                <h4 class="card-title">{props.title}</h4>
-                <p class="card-text">{props.desc}</p>
-                <a href={props.github} title="Github-Profile" id={props.id} target="_blank" rel="noopener noreferrer"><i className="fab fa-github"></i></a>
+
+            <div className="card-body">
+                <h4 className="card-title">{project.title}</h4>
+                <p className="card-text">{project.desc}</p>
+                <a href={project.github} title="Github-Profile" id={project._id} target="_blank" rel="noopener noreferrer"><i className="fab fa-github"></i></a>
             </div>
         </div>
+    ))
+
+    return (
+        projectsList
     );
 }
 
